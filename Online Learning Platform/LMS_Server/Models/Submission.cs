414faf8dc462d9e45dfd4819f38e857762d6c022
Shuaib Al-Bulushi <shuaib1151@gmail.com>
2026-08-04 12:55:03 +0400

    added constraints to submission

--- Online Learning Platform/LMS_Server/Models/Submission.cs
+++ Online Learning Platform/LMS_Server/Models/Submission.cs
@@ -1,17 +1,29 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace LMS_Server.Models
 {
     public class Submission
     {
+        [Key]
+        [JsonIgnore]
         public int SubmissionId { get; set; }
+
+        [Required]
         public string SubmissionContent { get; set; }
+
+        [Required]
         public string SubmissionGrade { get; set; }
 
 
         // 1:M submit relationship
         [ForeignKey("user")]
+        [Required]
         public int UserId { get; set; }
-        public User user { get; set; }
+
+
+        [JsonIgnore]
+        public User? user { get; set; }
     }
 }
