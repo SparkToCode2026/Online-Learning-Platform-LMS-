df2d77800837eda7b38b23b6abdf57ad9f3050b2
Shuaib Al-Bulushi <shuaib1151@gmail.com>
2026-08-04 11:33:56 +0400

    added DbSet for all models

--- Online Learning Platform/LMS_Server/ProjectContext.cs
+++ Online Learning Platform/LMS_Server/ProjectContext.cs
@@ -6,6 +6,17 @@ namespace LMS_Server
     public class ProjectContext : DbContext
     {
         public DbSet<User> users { get; set; }
+        public DbSet<InstructorProfile> instructorProfiles { get; set; }
+        public DbSet<Category> categories { get; set; }
+        public DbSet<Course> courses { get; set; }
+        public DbSet<Module> modules { get; set; }
+        public DbSet<Lesson> lessons { get; set; }
+        public DbSet<Enrollment> enrollments { get; set; }
+        public DbSet<Assignment> assignments { get; set; }
+        public DbSet<Submission> submissions { get; set; }
+        public DbSet<Quiz> quizzes { get; set; }
+        public DbSet<QuizAttempt> quizAttempts { get; set; }
+        public DbSet<Certificate> certificates { get; set; }
 
         public ProjectContext(DbContextOptions<ProjectContext> options) : base(options)
         {
