e16e504bf396be9f0bd062f3a258c7b345bfa1ec
HibaYaqoob <hebaalmusalami@gmail.com>
2026-08-05 23:28:10 +0400

    1. POST: Create new course

--- Online Learning Platform/LMS_Server/Controllers/CourseController.cs
+++ Online Learning Platform/LMS_Server/Controllers/CourseController.cs
@@ -1,9 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LMS_Server.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace LMS_Server.Controllers
 {
-    public class CourseController
+    [ApiController]
+    [Route("Course")]
+    public class CourseController : ControllerBase
     {
+        private ProjectContext context;
+
+        public CourseController(ProjectContext _context)
+        {
+            context = _context;
+        }
+
+        // 1. POST: Create new course
+        [HttpPost("AddCourse")]
+        public IActionResult AddCourse(Course course)
+        {
+            context.courses.Add(course);
+            context.SaveChanges();
+
+            return Ok(course.CourseId);
+        }
+
     }
 }
