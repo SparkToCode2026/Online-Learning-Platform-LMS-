978904374d53d4b2493bddc4ba8a85d96fd26fa0
HibaYaqoob <hebaalmusalami@gmail.com>
2026-08-06 00:02:41 +0400

    Adding the Category Controller file

---
+++ Online Learning Platform/LMS_Server/Controllers/CategoryController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using LMS_Server.Models;
+namespace LMS_Server.Controllers
+{
+    [ApiController]
+    [Route("Category")]
+
+    public class CategoryController : ControllerBase
+    {
+        private ProjectContext context;
+
+
+        public CategoryController(ProjectContext _context)
+        {
+            context = _context;
+        }
+    }
+}
--- Online Learning Platform/LMS_Server/Controllers/CourseController.cs
+++ Online Learning Platform/LMS_Server/Controllers/CourseController.cs
@@ -1,6 +1,7 @@
 ﻿using LMS_Server.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using LMS_Server.Models;
 
 namespace LMS_Server.Controllers
 {
