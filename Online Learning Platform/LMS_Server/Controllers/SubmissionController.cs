003fddffc394917a2f12e3c092f53dbaee5dfd6a
Shuaib Al-Bulushi <shuaib1151@gmail.com>
2026-08-06 13:35:30 +0400

    added submission controller configuration

---
+++ Online Learning Platform/LMS_Server/Controllers/SubmissionController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LMS_Server.Controllers
+{
+    [Route("api/submission")]
+    [ApiController]
+    public class SubmissionController : ControllerBase
+    {
+        private readonly ProjectContext _context;
+
+        public SubmissionController(ProjectContext context)
+        {
+            _context = context;
+        }
+
+
+    }
+}
